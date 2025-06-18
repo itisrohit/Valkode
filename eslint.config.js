0b9a0374f6bb5f78234602e61e01fd007a606296
Rohit <dv6510@gmail.com>
2025-06-18 20:24:48 +0530

    addresses github workflow issue

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    extends: [
+      'eslint:recommended',
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedRequiringTypeChecking,
+    ],
+    parserOptions: {
+      project: './tsconfig.json',
+      tsconfigRootDir: import.meta.dirname,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/prefer-const": "error"
+    },
+    ignores: ['node_modules', 'dist'],
+  }
+);
